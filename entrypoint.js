10300c5d2882b826675fa89c3c26dd8e8234a126
Thomas Lebeau <lebeau.thomas@gmail.com>
2019-03-16 20:56:45 +0100

    ad

--- entrypoint.js
+++ entrypoint.js
@@ -1,22 +1,28 @@
 const { Toolkit } = require('actions-toolkit');
+
 const tools = new Toolkit();
 
 async function lint(to, from, convention = '@commitlint/config-conventional') {
-  return tools.runInWorkspace(
-    'commitlint',
-    ['-e', `-x ${convention}` , `--from ${from}`, `--to ${to}`]
-  );
+    const CMD = 'commitlint'
+    const ARGS = ['-e', `-x ${convention}`]
+
+    if (to) ARGS.push(`--to ${to}`);
+    if (from) ARGS.push(`--from ${from}`);
+
+    return tools.runInWorkspace(CMD, ARGS);
 }
 
 async function main() {
-  const {after: to, before: from} = tools.context.payload;
+    const {after: from, before: to} = tools.context.payload;
 
-  tools.log('Lint commits:');
-  tools.log(`  - To: ${to}`);
-  tools.log(`  - From: ${from}`);
+    tools.log('Lint commits:');
+    tools.log(`  - To: ${to}`);
+    tools.log(`  - From: ${from}`);
 
-  const linted = await lint(to, from);
-  tools.log(`linted`);
+    const linted = await lint(to, from);
 }
 
-main();
+main()
+    .then(tools.exit.success)
+    .catch(tools.log.fatal)
+    .then(tools.exit.failure)
