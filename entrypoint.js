fa9a5c57ecd6835b5dcb7b73feeef9d7646c571f
Thomas Lebeau <lebeau.thomas@gmail.com>
2019-03-16 19:09:40 +0100

    fix: split commit by lines

--- entrypoint.js
+++ entrypoint.js
@@ -1,12 +1,13 @@
 const { Toolkit } = require('actions-toolkit');
 const tools = new Toolkit();
 
+const N ='\n';
 const GIT = 'git';
 const GIT_ARGS = ['rev-list', '--simplify-by-decoration', '-2 HEAD'];
 const COMMITLINT = 'commitling'
 const COMMITLINT_ARGS = ['-e', '-x @commitlint/config-conventional']
 
-const commits = tools.runInWorkspace(GIT, ...GIT_ARGS);
+const commits = tools.runInWorkspace(GIT, ...GIT_ARGS).split(N);
 const [to] = commits;
 const [from] = commits.reverse();
 
