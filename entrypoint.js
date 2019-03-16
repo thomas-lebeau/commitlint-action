11de3d25dddbfeee18b4cce979c8d64af4a1efac
Thomas Lebeau <lebeau.thomas@gmail.com>
2019-03-16 19:05:11 +0100

    feat: run commitlint

--- entrypoint.js
+++ entrypoint.js
@@ -2,7 +2,9 @@ const { Toolkit } = require('actions-toolkit');
 const tools = new Toolkit();
 
 const GIT = 'git';
-const ARGS = ['rev-list', '--simplify-by-decoration', '-2', 'HEAD'];
+const GIT_ARGS = ['rev-list', '--simplify-by-decoration', '-2 HEAD'];
+const COMMITLINT = 'commitling'
+const COMMITLINT_ARGS = ['-e', '-x @commitlint/config-conventional']
 
 const commits = tools.runInWorkspace(GIT, ...ARGS);
 const [to] = commits;
@@ -11,3 +13,7 @@ const [from] = commits.reverse();
 tools.log('Lint commits:');
 tools.log(`  - To: ${to}`);
 tools.log(`  - From: ${from}`);
+
+tools.runInWorkspace(COMMIT_LINT, [...COMMITLINT_ARGS, `--from ${from}`, `--to ${to}`]);
+
+tools.log('Done!');
